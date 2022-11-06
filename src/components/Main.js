a0a5cf97ce419667c8e1cbfe18fc69d70fe0687f
rahmadsubakti <38337821+rahmadsubakti@users.noreply.github.com>
2022-11-06 18:22:26 +0700

    Cleaned unnecessary code

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,110 +1,12 @@
-import { useState, useReducer, useRef } from 'react';
+import { useState, useRef } from 'react';
 
-//import { ThemeSwitcher } from './units';
 import KeyPadSection from './KeyPadSection'; 
 import { Display } from './units';
 import { HeaderCalcSection } from './HeaderSection';
 
-import { ThemeContext, ThemeSwitchContext, handleNumberContext, handleInputContext } from './MainContext';
-import { ThemeSwitcher } from './units';
+import { ThemeContext, ThemeSwitchContext, handleInputContext } from './MainContext';
 
 import './_Main.scss';
-
-/*const inputReducer = (dispVal, action) => {
-  switch (action.type) {
-    case 'del':
-      return dispVal.slice(0, dispVal.length-1);
-    case 'reset':
-      return "";
-    case 'number':
-      let v;
-      if (action.signal.current) {
-        action.signal.current = null;
-        //console.log(action.signal.current);
-        //console.log(action.value);
-        v = action.value;
-        console.log(v);
-      }
-      console.log(v);
-      return v;
-  }
-}
-
-const FMain = () => {
-  const [theme, setTheme] = useState("1");
-  const [dispVal, dispValDispatch] = useReducer(inputReducer, "");
-  //const [dispVal, setDispVal] = useState("");
-  const [operator, setOperator] = useState("");
-
-  const signalRef = useRef(null); // for when operator is present
-
-  const handleTheme = e => {
-    const value = e.target.value;
-    setTheme(value);
-  }
-
-  /*const getNumber = e => {
-    if (signalRef.current) {
-      signalRef.current = null;
-      console.log(signalRef.current);
-      setDispVal(e.target.value);
-    } else {
-      console.log('concat number');
-      setDispVal(dispVal.concat(e.target.value));
-    }
-  }
-
-  const handleOperation = () => {
-    signalRef.current = "y";
-    console.log(signalRef.current);
-  }
-  
-  /*const handleOperation = e => {
-    let value = parseFloat(dispVal);
-    if (operator === "") {
-      setTotal(total => value);
-      setOperator(operator => "+")
-    } else {
-      value += total;
-      setTotal(total => value);
-      dispValDispatch({type: 'idk', value:value});
-    }
-    //let res;
-    if (operation === "") {
-      setTotal(total => dispVal);
-      setOperation(operation => e.target.value);
-    } else {
-      if (e.target.value == "+") {
-        value += total;
-        console.log(value);
-        setTotal(total => value);
-        dispValDispatch({type: 'idk', value: value})
-      }
-    }
-  }
-
-  return (
-    <ThemeSwitchContext.Provider value={handleTheme}>
-      <ThemeContext.Provider value={theme}>
-        <main className={"main-theme" + theme}>
-          <HeaderCalcSection />
-          <Display value={dispVal} />
-          <handleInputContext.Provider value={dispValDispatch}>
-            <KeyPadSection handleOperation={handleOperation} signalRef={signalRef}/>
-          </handleInputContext.Provider>
-        </main>
-      </ThemeContext.Provider>
-    </ThemeSwitchContext.Provider>
-  )
-}*/
-
-const divide = (num1, num2) => {
-  const result = num1/num2;
-  if (!isFinite(result)) {
-    throw "Cannot be divided by zero"
-  }
-  return result;
-}
 
 const Main = () => {
   const init = {
@@ -193,30 +95,6 @@ const Main = () => {
 
     signalRef.current = true;
   }
-  
-  /*const handleOperation = e => {
-    let value = parseFloat(dispVal);
-    if (operator === "") {
-      setTotal(total => value);
-      setOperator(operator => "+")
-    } else {
-      value += total;
-      setTotal(total => value);
-      dispValDispatch({type: 'idk', value:value});
-    }
-    //let res;
-    if (operation === "") {
-      setTotal(total => dispVal);
-      setOperation(operation => e.target.value);
-    } else {
-      if (e.target.value == "+") {
-        value += total;
-        console.log(value);
-        setTotal(total => value);
-        dispValDispatch({type: 'idk', value: value})
-      }
-    }
-  }*/
 
   const handleInput = {
     "handleDel" : handleDel,
